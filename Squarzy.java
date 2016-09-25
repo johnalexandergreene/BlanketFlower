ab81de560d72a8d60be63663d04ef8cb5ae70d63
johnalexandergreene <john@fleen.org>
2016-09-25 16:15:20 -0500

    init commit

--- Squarzy.java
+++ Squarzy.java
@@ -1,5 +1,11 @@
-# Squarzy
+package org.fleen.squarzy;
 
-shape grammar fractal (like forsythia, except square instead of kisrhombille)
+/*
+ * shape grammar fractal (like forsythia, except square instead of kisrhombille)
  * All parametric jigs with specific classes. Each shape class has its own specific jigs (with lots of identification via tags for filtering etc)
  * Small set of shape classes. Just 6 or so
+ * 
+ */
+public class Squarzy{
+
+}
